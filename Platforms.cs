ecafb924dd3f9166f1f0d0de776746528bd2ce5b
Nima abdoli <nimaabdoli@outlook.com>
2022-09-13 02:46:30 +0430

    add OsLookUp to platform class but it will be changed

--- Platforms.cs
+++ Platforms.cs
@@ -1,6 +1,29 @@
+using System;
+
 namespace Xdit
 {
-   enum Platforms
+    public class Platforms
+    {
+        public PlatformsEnum platform;
+         // TODO : Must use constructor exept OsLook method. so the class define platform when class instantioted.
+
+        // Check for which platform(Os, kernel) are used.  
+        public PlatformsEnum OsLook()
+        {
+           if (OperatingSystem.IsWindows())
+            {
+                platform = PlatformsEnum.Windows;
+                return PlatformsEnum.Windows;
+            }
+           else
+            {
+                platform = PlatformsEnum.linux;
+                return PlatformsEnum.linux;
+            }
+        }// end of OsLook Method.
+    }
+
+    public enum PlatformsEnum
         {
             Windows = 1,
             linux,
