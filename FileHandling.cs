f1a906a8d93b5aafc329a2beb4e0ee337b35912f
Nima abdoli <nimaabdoli@outlook.com>
2022-09-15 23:02:13 +0430

    Code Clean up in FileHandling Class

--- FileHandling.cs
+++ FileHandling.cs
@@ -7,28 +7,49 @@ namespace Xdit
 {
     public class FileHandling
     {
+
+        #region Public Property
+
+        // Save Complete location of file based on platform
         public string FileLocation{get; set;}
+
+        // Save the path of directory where app called from terminal.
         public string WorkingDirectory{get; set;}
 
+        #endregion end Public Property
 
+        #region Private Property
+
+        // save argument thay come to app
         private string[] ArgsIn{get; set;}
-        public bool IsRoot;
+
+        // make sure that working directory is in top of directories hierarchy or not. for complaication may occur in diffrent path format in diffrent platform 
+        public bool IsRoot; 
+
+        #endregion end Private Property
         
+        // constructor to get argument from terminal(entry) and handle all diffrent scenario that will happend to entry argument.
         public FileHandling(string[] args)
         {
+            // get the directory of where app called or run.
             WorkingDirectory = Environment.CurrentDirectory;
             
+            // check if there is argument in array.
             if (args.Length != 0)
-            {
+            {   
+                // this may be deleted in test always get file but maybe app have other argument to
                 ArgsIn = args;
+                // path to the file.
                 FileLocation = WorkingDirectory + fileNameTrim(ArgsIn[0]);
             }
-        }
+        }// end of FileHandling Constructor.
 
-
+        // TODO : need linux and window file handling
+        // trim the file path. windows use format like this {.\file.ext} thepoint must be deleted. linux handle it in other way.
         string fileNameTrim(string fileName)
         {
                 return fileName.Trim('.');
-        }    
-    }
-}
+        }// end of fileNameTrim  
+
+    }// end of FileHandling Class
+}// end of Xdit Namespace
