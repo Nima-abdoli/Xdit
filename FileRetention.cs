35ff87681a46bf03e52fe91779c8fad267c30dbb
Nima abdoli <nimaabdoli@outlook.com>
2022-09-30 22:52:18 +0330

    FileRetention

---
+++ FileRetention.cs
@@ -0,0 +1,9 @@
+namespace Xdit
+{
+    //TODO : this class save paths of the files that open or create by editor
+    public class FileRetention
+    {
+        
+    }// end od FileRetention Class
+    
+}// end of Xdit Namespace
